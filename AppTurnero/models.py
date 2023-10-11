99a18a34f66be64043ee44abe35ab7c9f0c906f1
Mariana Torres <mariandreatorres@gmail.com>
2023-10-11 04:57:38 -0300

    Forms LOGIN

--- AppTurnero/models.py
+++ AppTurnero/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class DatosProfesionales(models.Model):
     def __str__(self):
 
-        return f"nombre: {self.nombre}------ Apellido:{self.apellido}------ Mail: {self.mail}------ Cuit: {self.cuit}   ------ Email: {self.email}------ Razon Social: {self.razon_social} ------ Especialidad: {self.especialidad}"
+        return f"Nombre: {self.nombre} -- Apellido:{self.apellido} -- Mail: {self.mail} -- Cuit: {self.cuit} -- Razon Social: {self.razon_social}  --  Especialidad: {self.especialidad}"
     id_profesional = models.AutoField(primary_key=True)
     nombre = models.CharField(default="", max_length=60)
     apellido = models.CharField(default="", max_length=60)
@@ -14,18 +14,28 @@ class DatosProfesionales(models.Model):
     especialidad = models.CharField(default="", max_length=60)
 
 class HorariosProfesionales(models.Model):
+    def __str__(self):
+
+        return f"id_profesional: {self.id_profesional} -- Dia de la semana: {self.dia_semana} -- hora_inicio: {self.hora_inicio} -- hora_fin: {self.hora_fin} "
     id_profesional = models.ForeignKey(DatosProfesionales, on_delete=models.CASCADE)
     dia_semana = models.CharField(default="", max_length=10)
     hora_inicio = models.TimeField(default=datetime.now)
     hora_fin = models.TimeField(default=datetime.now)
 
 class Meses(models.Model):
+    def __str__(self):
+
+        return f"periodo: {self.periodo} -- id_profesional: {self.id_profesional} -- fecha: {self.fecha}  -- hora: {self.hora}"
+
     periodo = models.CharField(default="202301", max_length=10)
     id_profesional = models.ForeignKey(DatosProfesionales, on_delete=models.CASCADE)
     fecha = models.DateField(default=datetime.today)
     hora = models.TimeField(default=datetime.now)
 
 class Pacientes(models.Model):
+    def __str__(self):
+
+        return f"id_paciente: {self.id_paciente} -- nombre: {self.nombre} -- apellido: {self.apellido} -- obra social: {self.obra_social} -- numero obra social: {self.numero_os} -- avatar: {self.avatar}"
     id_paciente = models.AutoField(primary_key=True)
     nombre = models.CharField(default="", max_length=60)
     apellido = models.CharField(default="", max_length=60)
